7cd46219f08b03fdc6cfef417bddafba0f6aa6c6
Wahid Ali <wahidali8566@gmail.com>
2024-10-26 17:01:29 +0530

    setup for backend

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_, res) => {
+  return res.status(200).json({
+    message: "i am coming from backened",
+    success: true,
+  });
+});
+
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+
+const corsOption = {
+  origin: "http://localhost:5173",
+  Credentials: true,
+};
+
+app.use(cors(corsOption));
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`app is running on ${PORT}`);
+});
+
+//231894ali
