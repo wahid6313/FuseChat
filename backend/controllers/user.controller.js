7a62be582fa69a4999d55d6099a0200750e34f3f
Wahid Ali <wahidali8566@gmail.com>
2024-10-29 19:44:42 +0530

    controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -201,12 +201,3 @@ export const followOrUnfollow = async (req, res) => {
     console.log(error);
   }
 };
-
-export const likesOrUnlikes = async (req, res) => {
-  const userId = req.body;
-
-  res.status(200).json({
-    message: "you like someomne",
-    success: true,
-  });
-};
