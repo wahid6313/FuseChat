72efc6d45fe7ab982cfac012fcab1ab96731d041
Wahid Ali <wahidali8566@gmail.com>
2024-11-01 19:16:52 +0530

    Post Controller complete

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -98,3 +98,197 @@ export const getUserPost = async (req, res) => {
     console.log(error);
   }
 };
+
+export const likePost = async (req, res) => {
+  try {
+    const likeKarneWalaUserId = req.id;
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "There is no Post",
+        success: false,
+      });
+    }
+
+    //likes logic------
+    await post.updateOne({ $addToSet: { likes: likeKarneWalaUserId } });
+    await post.save();
+
+    //implementing socket io for real time notifications----
+
+    return res.status(200).json({
+      message: "Post Liked",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const disLikePost = async (req, res) => {
+  try {
+    const disLikeKarneWalaUserId = req.id;
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "There is no Post",
+        success: false,
+      });
+    }
+
+    //likes logic------
+    await post.updateOne({ $pull: { likes: disLikeKarneWalaUserId } });
+    await post.save();
+
+    //implementing socket io for real time notifications----
+
+    return res.status(200).json({
+      message: "Post Disliked",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addComment = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const commentKarneWalaUserKiId = req.id;
+    const { text } = req.body;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Text is required",
+        success: false,
+      });
+    }
+    const comment = await Comment.create({
+      text,
+      auhtor: commentKarneWalaUserKiId,
+      post: postId,
+    }).populate({ path: "auhtor", select: "userName, profilePicture" });
+
+    post.comment.push(comment._id);
+    await post.save();
+
+    return res.status(200).json({
+      message: "Successfully Commented ",
+      comment,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCommentOfPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const comments = await Comment.find({ post: postId }).populate(
+      "author",
+      "userName, profilePicture"
+    );
+
+    if (!comments) {
+      return res.status(404).json({
+        message: "No comment Found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      comments,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const authorId = req.id;
+
+    const post = await Post.find(postId);
+    if (!post) {
+      return res.status(404).json({
+        message: "No post Found",
+        success: false,
+      });
+    }
+
+    //check if the loged in user is the owner of the post-----
+
+    if (post.auhtor.toString() != authorId) {
+      return res.status(403).json({
+        message: "You are not auhtorized to delete this post",
+        success: false,
+      });
+    }
+
+    //delete post----------
+    await Post.findByIdAndDelete(postId);
+
+    //remove the post id from user post----
+
+    let user = await Post.findById(authorId);
+    user.posts = user.posts.filter((id) => id.toString() != postId);
+    await user.save();
+
+    //delete associated comments------
+    await Comment.deleteMany({ post: postId });
+
+    return res.status(200).json({
+      message: "Post Deleted",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const bookmarkPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const authorId = req.id;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+        success: false,
+      });
+    }
+
+    const user = await Post.findById(authorId);
+    if (user.bookmarks.includes(post._id)) {
+      //already bookes so removed from bookmarks--------
+      await user.updateOne({ $pull: { bookmarks: post._id } });
+      await user.save();
+      return res.status(200).json({
+        type: "unsaved",
+        message: "Post removed from bookmarks",
+        success: true,
+      });
+    } else {
+      //bookmaked karna padega---------------
+      await user.updateOne({ $addToSet: { bookmarks: post._id } });
+      await user.save();
+      return res.status(200).json({
+        type: "saved",
+        message: "Post added to bookmarks",
+        success: true,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
