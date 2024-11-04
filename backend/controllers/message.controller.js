3400ea0167e13b4fc91efabf64218b2d41b87d49
Wahid Ali <wahidali8566@gmail.com>
2024-11-04 21:00:07 +0530

    message  controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,28 +1,28 @@
-// import Conversation from "../models/conversation.model.js";
-// import Message from "../models/message.model.js";
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
-// export const sendMessage = async (req, res) => {
-//   try {
-//     const senderId = req.id;
-//     const receiverId = req.params.id;
-//     const { message } = req.body;
+export const sendMessage = async (req, res) => {
+  try {
+    const senderId = req.id;
+    const receiverId = req.params.id;
+    const { message } = req.body;
 
-//     let conversation = await Conversation.findOne({
-//       participants: { $all: [senderId, receiverId] },
-//     });
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
 
-//     //establish if conversation if not started yet---
-//     if (!conversation) {
-//       conversation = await Conversation.create({
-//         participants: [senderId, receiverId],
-//       });
-//     }
-//     const newMessage = await Message.create({
-//       senderId,
-//       receiverId,
-//       message,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+    //establish if conversation if not started yet---
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      message,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
