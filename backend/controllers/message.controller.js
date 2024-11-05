74bc2116d06c616a4dc2d6f344c6badcecbd2267
Wahid Ali <wahidali8566@gmail.com>
2024-11-05 11:56:49 +0530

    message controller complete

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -22,6 +22,39 @@ export const sendMessage = async (req, res) => {
       receiverId,
       message,
     });
+    if (newMessage) conversation.messages.push(newMessage._id);
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    //implement socket io for real time data transfer------
+
+    return res.status(200).json({
+      success: true,
+      newMessage,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const senderId = req.id;
+    const receiverId = req.params.id;
+    const conversation = await Conversation.find({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      return res.status(200).json({
+        success: true,
+        messages: [],
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      messages: conversation?.messages,
+    });
   } catch (error) {
     console.log(error);
   }
--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -8,7 +8,7 @@ const conversationSchema = new mongoose.Schema({
     },
   ],
 
-  message: [
+  messages: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Message",
